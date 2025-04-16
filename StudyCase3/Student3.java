d37267ea6b8fea312e6d6a935b38d33c2ab22cf0
Devin R <dvnrt.25@gmail.com>
2025-04-16 17:13:53 +0700

    Add files via upload
    Group 3 Study Case 3

---
+++ StudyCase3/Student3.java
@@ -0,0 +1,12 @@
+public class Student3 {
+    String nim, name;
+
+    public Student3(String nim, String name){
+        this.nim=nim;
+        this.name=name;
+    }
+
+    public void displayDataStd(){
+        System.out.println("NIM: "+nim+" | Name: "+name);
+    }
+}
